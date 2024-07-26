31d4c8d1abfa4a37d545d341e553cfcda43aad64
jamesycode <jamesycode@gmail.com>
2024-07-26 16:43:26 -0400

    added login and signup form.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 // Import necessary modules from React library
 import React, { useEffect } from 'react';
-import Landing_Page from './Components/Landing_Page/Landing_Page.js';
-
-
 // Import components for routing from react-router-dom library
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 // Import custom Navbar component
 import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_Page.js';
+import Login from './Components/Login/Login.jsx';
+import Sign_Up from './Components/Sign_Up/Sign_Up.jsx';
 
 // Function component for the main App
 function App() {
 
-    // Render the main App component
-    return (
-        <div className="App">
-            {/* Set up BrowserRouter for routing */}
-            <BrowserRouter>
-                {/* Display the Navbar component */}
-                <Navbar />
-
-                {/* Set up the Routes for different pages */}
-                <Routes>
-                    <Route path="/" element={<Landing_Page />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+	// Render the main App component
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Landing_Page />} />
+					<Route path="/Login" element={<Login />} />
+					<Route path="/Signup" element={<Sign_Up />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 // Export the App component as the default export
